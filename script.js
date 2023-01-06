e3fc62346c8f975643e1d71bfd03287cbb0cc98a
vertalvaro <alvarmiguelmart@gmail.com>
2023-01-05 23:35:19 -0300

    feat: add javascript

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-function select() {
-    let selecionar = document.getElementById("")
-    selecionar.style.border = "5px solid green";
-
+function selectDishes(select) {
+    let unselect = document.querySelector(".dishes .selected")
+    if (unselect !== null) {
+        unselect.classList.remove("selected")
+    }
+    select.classList.add("selected");
+    checkOrderCondition();
+}
+function selectDrinks(select) {
+    let unselect = document.querySelector(".drinks .selected")
+    if (unselect !== null) {
+        unselect.classList.remove("selected")
+    }
+    select.classList.add("selected");
+    checkOrderCondition();
+}
+function selectDesserts(select) {
+    let unselect = document.querySelector(".desserts .selected")
+    if (unselect !== null) {
+        unselect.classList.remove("selected")
+    }
+    select.classList.add("selected");
+    checkOrderCondition();
+}
+function checkOrderCondition() {
+    const selectedDish = document.querySelector(".dishes .selected")
+    const selectedDrink = document.querySelector(".drinks .selected")
+    const selectedDessert = document.querySelector(".desserts .selected")
+    if (selectedDish !== null && selectedDrink !== null && selectedDessert !== null) {
+        const butao = document.querySelector("footer button")
+        butao.classList.add("activated")
+        butao.innerHTML = "Fechar pedido"
+        butao.disabled = false
+    }
+}
+function showModal() {
+    const show = document.querySelector(".modal")
+    show.style.display = "flex"
+}
+function hideModal() {
+    const hide = document.querySelector(".modal")
+    hide.style.display = "none"
 }
