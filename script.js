9b11b6b3ef678bd8b0da018606ce9cb0ca4b55b9
vertalvaro <alvarmiguelmart@gmail.com>
2023-01-08 17:25:48 -0300

    feat: add bonus and functionality

--- script.js
+++ script.js
@@ -4,24 +4,65 @@ function selectDishes(select) {
     unselect.classList.remove("selected");
   }
   select.classList.add("selected");
+  dishName = document.querySelector(".dishes .selected h3").innerHTML;
+  dishPrice = document
+    .querySelector(".dishes .selected h4")
+    .innerHTML.replace(/,/g, ".")
+    .replace(/^.{3}/g, "");
+  document.getElementById("firstItem").innerHTML = dishName;
+  document.getElementById("firstPrice").innerHTML = dishPrice.replace(".", ",");
   checkOrderCondition();
 }
+
+let dishName;
+let dishPrice;
+
 function selectDrinks(select) {
   const unselect = document.querySelector(".drinks .selected");
   if (unselect !== null) {
     unselect.classList.remove("selected");
   }
   select.classList.add("selected");
+  drinkName = document.querySelector(".drinks .selected h3").innerHTML;
+  drinkPrice = document
+    .querySelector(".drinks .selected h4")
+    .innerHTML.replace(/,/g, ".")
+    .replace(/^.{3}/g, "");
+  document.getElementById("secondItem").innerHTML = drinkName;
+  document.getElementById("secondPrice").innerHTML = drinkPrice.replace(
+    ".",
+    ","
+  );
   checkOrderCondition();
 }
+
+let drinkName;
+let drinkPrice;
+
 function selectDesserts(select) {
   const unselect = document.querySelector(".desserts .selected");
   if (unselect !== null) {
     unselect.classList.remove("selected");
   }
   select.classList.add("selected");
+  dessertName = document.querySelector(".desserts .selected h3").innerHTML;
+  dessertPrice = document
+    .querySelector(".desserts .selected h4")
+    .innerHTML.replace(/,/g, ".")
+    .replace(/^.{3}/g, "");
+  document.getElementById("thirdItem").innerHTML = dessertName;
+  document.getElementById("thirdPrice").innerHTML = dessertPrice.replace(
+    ".",
+    ","
+  );
   checkOrderCondition();
 }
+
+let clientName;
+let clientAdress;
+let dessertName;
+let dessertPrice;
+
 function checkOrderCondition() {
   const selectedDish = document.querySelector(".dishes .selected");
   const selectedDrink = document.querySelector(".drinks .selected");
@@ -37,25 +78,52 @@ function checkOrderCondition() {
     butao.style.fontWeight = "700";
     butao.style.fontSize = "20px";
     butao.disabled = false;
-    changeURL();
+    document.getElementById("total").innerHTML =
+      "R$ " +
+      (
+        parseFloat(dishPrice) +
+        parseFloat(drinkPrice) +
+        parseFloat(dessertPrice)
+      )
+        .toFixed(2)
+        .replace(".", ",");
   }
 }
+
 function showModal() {
   const show = document.querySelector(".modal");
   show.style.display = "flex";
+  askName();
+  askAdress();
+  changeURL();
 }
+
 function hideModal() {
   const hide = document.querySelector(".modal");
   hide.style.display = "none";
 }
 
+function askName() {
+  clientName = prompt("Digite seu nome");
+  return;
+}
+
+function askAdress() {
+  clientAdress = prompt("Digite seu endereço para entrega");
+  return;
+}
+
 function changeURL() {
   const params = encodeURIComponent(
     `Olá, gostaria de fazer o pedido: 
-    - Prato: Frango Yin Yang 
-    - Bebida: Coquinha Gelada 
-    - Sobremesa: Pudim 
-    Total: R$ ${(27.7).toFixed(2)}`
+    - Prato: ${dishName} 
+    - Bebida: ${drinkName}
+    - Sobremesa: ${dessertName}
+    Total: R$ ${
+      parseFloat(dishPrice) + parseFloat(drinkPrice) + parseFloat(dessertPrice)
+    }
+    Nome: ${clientName}
+    Endereço: ${clientAdress}`
   );
   const url = "https://wa.me/5511958174303?text=" + params;
   document.getElementById("link").href = url;
