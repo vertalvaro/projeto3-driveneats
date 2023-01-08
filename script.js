c73b49d2804ef2bb48baedc361ea9159a60887d8
vertalvaro <alvarmiguelmart@gmail.com>
2023-01-07 23:37:00 -0300

    fix: minor changes

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 function selectDishes(select) {
-    let unselect = document.querySelector(".dishes .selected")
-    if (unselect !== null) {
-        unselect.classList.remove("selected")
-    }
-    select.classList.add("selected");
-    checkOrderCondition();
+  const unselect = document.querySelector(".dishes .selected");
+  if (unselect !== null) {
+    unselect.classList.remove("selected");
+  }
+  select.classList.add("selected");
+  checkOrderCondition();
 }
 function selectDrinks(select) {
-    let unselect = document.querySelector(".drinks .selected")
-    if (unselect !== null) {
-        unselect.classList.remove("selected")
-    }
-    select.classList.add("selected");
-    checkOrderCondition();
+  const unselect = document.querySelector(".drinks .selected");
+  if (unselect !== null) {
+    unselect.classList.remove("selected");
+  }
+  select.classList.add("selected");
+  checkOrderCondition();
 }
 function selectDesserts(select) {
-    let unselect = document.querySelector(".desserts .selected")
-    if (unselect !== null) {
-        unselect.classList.remove("selected")
-    }
-    select.classList.add("selected");
-    checkOrderCondition();
+  const unselect = document.querySelector(".desserts .selected");
+  if (unselect !== null) {
+    unselect.classList.remove("selected");
+  }
+  select.classList.add("selected");
+  checkOrderCondition();
 }
 function checkOrderCondition() {
-    const selectedDish = document.querySelector(".dishes .selected")
-    const selectedDrink = document.querySelector(".drinks .selected")
-    const selectedDessert = document.querySelector(".desserts .selected")
-    if (selectedDish !== null && selectedDrink !== null && selectedDessert !== null) {
-        const butao = document.querySelector("footer button")
-        butao.classList.add("activated")
-        butao.innerHTML = "Fechar pedido"
-        butao.disabled = false
-    }
+  const selectedDish = document.querySelector(".dishes .selected");
+  const selectedDrink = document.querySelector(".drinks .selected");
+  const selectedDessert = document.querySelector(".desserts .selected");
+  if (
+    selectedDish !== null &&
+    selectedDrink !== null &&
+    selectedDessert !== null
+  ) {
+    const butao = document.querySelector("footer button");
+    butao.classList.add("activated");
+    butao.innerHTML = "Fechar pedido";
+    butao.style.fontWeight = "700";
+    butao.style.fontSize = "20px";
+    butao.disabled = false;
+  }
 }
 function showModal() {
-    const show = document.querySelector(".modal")
-    show.style.display = "flex"
+  const show = document.querySelector(".modal");
+  show.style.display = "flex";
 }
 function hideModal() {
-    const hide = document.querySelector(".modal")
-    hide.style.display = "none"
+  const hide = document.querySelector(".modal");
+  hide.style.display = "none";
 }
